a19f21fdc253a3557d7c91b0655cd784142ebbbd
WansleiAraujo <36192039+WansleiAraujo@users.noreply.github.com>
2021-09-09 13:56:07 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,8 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<html>
+ <head>
+  <title>Teste PHP</title>
+ </head>
+ <body>
+ <?php echo "<p>Olá Mundo</p>"; ?>
+ </body>
+</html>
